4befe2b37659f0a298c9b2ee5d859d694e1d37c4
soheib <bouafiasoheib918@gmail.com>
2025-03-02 13:33:41 +0100

    change

--- js/script.js
+++ js/script.js
@@ -15,11 +15,4 @@ document.addEventListener("DOMContentLoaded", () => {
 // Header phone click 
 
 
-document.addEventListener("DOMContentLoaded", function () {
-    const menuIcon = document.querySelector(".menu-icon");
-    const menu = document.querySelector(".menu");
 
-    menuIcon.addEventListener("click", function () {
-        menu.classList.toggle("show");
-    });
-});
