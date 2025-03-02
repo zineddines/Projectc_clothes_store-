2575dbe6f2c03ba9e2b8def4b374dc0b010bfb3b
soheib <bouafiasoheib918@gmail.com>
2025-03-02 12:59:02 +0100

    change

--- js/script.js
+++ js/script.js
@@ -9,3 +9,17 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+
+
+
+// Header phone click 
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.querySelector(".menu-icon");
+    const menu = document.querySelector(".menu");
+
+    menuIcon.addEventListener("click", function () {
+        menu.classList.toggle("show");
+    });
+});
