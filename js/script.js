9ffe2ee07a6c08554e7fc2faa4301536b8e9f630
soheib <bouafiasoheib918@gmail.com>
2025-03-02 23:31:13 +0100

    change

--- js/script.js
+++ js/script.js
@@ -14,5 +14,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // Header phone click 
 
+const menu_head =document.getElementById("menico");
+const list_head=document.getElementById("listhead");
+const exitmenu=document.getElementById("exitmenu");
 
+
+function dispadd(){
+
+list_head.classList.add("show");
+
+}
+
+function disprv(){
+
+    list_head.classList.remove("show");
+    
+    }
 
